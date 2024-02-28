4cb14ac5d2b3bb18cc49ad243de9302304b202a8
Shekhar Ashok Mohite <122733304+shekharma@users.noreply.github.com>
2024-02-28 19:30:58 +0530

    Add files via upload

---
+++ 12. Activation Functions.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# option 1 (create nn modules)
+
+class NeuralNet(nn.module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.linear1=nn.Linear(input_size,hidden_size)
+        self.relu=nn.ReLU()
+        self.linear2=nn.Linear(hidden_size,1)
+        self.sigmoid=nn.Sigmoid()
+        
+    def forwar(self,x):
+        out=self.linear1(x)
+        out=self.relu(out)
+        out=self.linear2(out)
+        out=self.sigmoid(out)
+        return out
+    
+    # option 2 (use activation functions directly in frwrd pass)
+    class NeuralNet(nn.Module):
+        def __init__(self, input_size, hidden_size):
+            super(NeuralNet,self).__init__()
+            self.linear1=nn.Linear(input_size,hidden_size)
+            self.linear2=nn.Linear(hidden_size,1)
+
+        def forward(self,x):
+            out=torch.relu(self.linear1(x))
+            out=torch.sigmoid(self.linear2(out))
+            return out
